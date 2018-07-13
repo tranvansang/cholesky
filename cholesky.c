6f0cd5f5786e3a855fa121b55243d2fe4f8d31e2
Tran Sang <tranvansangk41@gmail.com>
2018-07-13 13:37:52 +0900

    time measure ok

--- cholesky.c
+++ cholesky.c
@@ -4,7 +4,8 @@
 #include <assert.h>
 #include <mpi.h>
 
-#define ROOT_PROCESS 0
+#define ROOT_RANK 0
+#define RUN_SERIAL 1
 
 /* matrix-vector multiply : y = A * x, where 
    A is symmetric and only lower half are stored */
@@ -24,7 +25,7 @@ void symMatVec(int n, double *a, double *x, double *y) {
 }
 
 /* solve Ax = b */
-void solveSym(int rank, int nprocesses, int n, double *a, double *x, double *b) {
+void solveSym_serial(int n, double *a, double *x, double *b) {
   int i, j, k;
 
   /* LDLT decomposition: A = L * D * L^t */
@@ -62,6 +63,64 @@ void solveSym(int rank, int nprocesses, int n, double *a, double *x, double *b) 
   }
 }
 
+//matrix element - row major
+#define ELM(a, r, c, ld) (a)[(r) * (ld) + c]
+//matrix element - column major
+#define ELM_C(a, r, c, ld) (a)[r + (c) * (ld)]
+
+/* solve Ax = b */
+void solveSym(int rank, int nprocesses, int n, double *a, double *x, double *b) {
+  int i, j, k;
+  double *local_a = malloc(sizeof(double) * n * (n + nprocesses - 1) / nprocesses);
+  assert(local_a != NULL);
+  //deliver row data to each other processes from root process
+  for(i = 0; i < n; i++){
+  }
+
+  /* LDLT decomposition: A = L * D * L^t */
+  for (i=0; i< n; i++) {
+    double invp = 1.0 / a[i*n+i];
+
+    for (j= i+1; j< n; j++) {
+      double aji = a[j*n+i];
+      a[j*n+i] *= invp;
+
+      for (k= i+1; k<= j; k++)
+        a[j*n+k] -= aji * a[k*n+i];
+    }
+  }
+
+  /* forward solve L y = b: but y is stored in x
+     can be merged to the previous loop */
+  for (i=0; i< n; i++) {
+    double t = b[i];
+
+    for (j=0; j< i; j++)
+      t -= a[i*n+j] * x[j];
+
+    x[i] = t;
+  }
+
+  /* backward solve D L^t x = y */
+  for (i= n-1; i>= 0; i--) {
+    double t = x[i] / a[i*n+i];
+
+    for (j= i+1; j< n; j++)
+      t -= a[j*n+i] * x[j];
+
+    x[i] = t;
+  }
+}
+
+double norm(double *x, double* y, n){
+  /* check error norm */
+  double e = 0;
+  int i;
+  for (i=0; i< n; i++)
+    e += (x[i] - xx[i]) * (x[i] - xx[i]);
+  return sqrt(e);
+}
+
 int main(int argc, char **argv) {
   // Initialize the MPI environment
   MPI_Init(&argc, &argv);
@@ -77,9 +136,10 @@ int main(int argc, char **argv) {
   char processor_name[MPI_MAX_PROCESSOR_NAME];
   int name_len;
   int n, i, j;
-  double *a, *xx, *b, *x, e, s;
+  double *a, *xx, *b, *x, e, s, *a_copy, *b_copy;
+  double time_start, time_stop;
 
-  if (world_rank == ROOT_PROCESS){
+  if (world_rank == ROOT_RANK){
 
     // Get the name of the processor
     MPI_Get_processor_name(processor_name, &name_len);
@@ -120,27 +180,55 @@ int main(int argc, char **argv) {
     /* make right hand side b = Ax */
     b = malloc(sizeof(double) * n);
     assert(b != NULL);
-  }
-  MPI_Barrier();
 
-  symMatVec(n, a, xx, b);
+    symMatVec(n, a, xx, b);
 
-  if (world_rank == ROOT_PROCESS){
     /* solution vector, pretend to be unknown */
     x = malloc(sizeof(double) * n);
     assert(x != NULL);
+  }
 
-    /* solve: the main computation */
-    solveSym(world_rank, world_size, n, a, x, b);
+  if (RUN_SERIAL && world_rank == ROOT_RANK){
+    //clone data
+    a_copy = malloc(sizeof(double) * n * n);
+    assert(a_copy != NULL);
+    b_copy = malloc(sizeof(double) * n);
+    assert(b_copy != NULL);
+    memcpy(a_copy, a, sizeof(double) * n * n);
+    memcpy(b_copy, b, sizeof(double) * n);
 
-    /* check error norm */
-    e = 0;
-    for (i=0; i< n; i++)
-      e += (x[i] - xx[i]) * (x[i] - xx[i]);
-    e = sqrt(e);
+    //serial solver
+    time_start = mpi_wtime();
+    solveSym_serial(n, a_copy, x, b_copy);
+    time_stop = mpi_wtime();
+    printf("Serial time: %.8f sec\n", time_stop - time_start);
 
+    e = norm(x, xx, n);
     printf("error norm = %e\n", e);
     printf("--- good if error is around n * 1e-16 or less\n");
+
+    //free data
+    free(a_copy);
+    free(b_copy);
+    memset(x, 0, sizeof(double) * n);
+  }
+
+  MPI_Barrier();
+  //parallel version
+  time_start = mpi_wtime();
+  /* solve: the main computation */
+  solveSym(world_rank, world_size, n, a, x, b);
+  MPI_Barrier();
+  time_stop = mpi_wtime();
+
+  if (world_rank == ROOT_RANK){
+    printf("Paralle time: %.8f sec\n", time_stop - time_start);
+
+    e = norm(x, xx, n);
+    printf("error norm = %e\n", e);
+    printf("--- good if error is around n * 1e-16 or less\n");
+
+    //free data
     free(a);
     free(xx);
     free(b);
